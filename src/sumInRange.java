10417885c3ef57cf47e4f560c0240a0406271ceb
Ronyszx <ronysouz7@gmail.com>
2022-05-04 17:06:08 +0530

    100codes

---
+++ src/sumInRange.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class sumInRange {
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the initial number");
+        int a = s.nextInt();
+        System.out.println("Enter the final number");
+        int b = s.nextInt();
+        int sum = 0;
+
+        for(int i =a; i <= b; i++){
+            sum += i;
+        }
+        System.out.println("The sum is "+ sum);
+
+    }
+}
