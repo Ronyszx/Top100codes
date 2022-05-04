d45fac024bf2cea490c451ef87b9c6eef1491511
Ronyszx <ronysouz7@gmail.com>
2022-05-04 16:57:25 +0530

    100codes

--- src/SumNnaturalNos.java
+++ src/SumNnaturalNos.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class SumNnaturalNos {
+    public static void main(String args[]){
+
+        int sum = 0;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n = s.nextInt();
+
+        for(int i = 1; i <=n; i++){
+            sum += i;
+        }
+        System.out.println(sum);
+    }
 }
