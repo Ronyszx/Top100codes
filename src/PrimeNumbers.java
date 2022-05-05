27ffee86745346c8f6abb8409d56a2ce36a4a74e
Ronyszx <ronysouz7@gmail.com>
2022-05-05 19:17:15 +0530

    100codes

---
+++ src/PrimeNumbers.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class PrimeNumbers {
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the number to check:");
+        int n = s.nextInt();
+
+        primeno(n);
+    }
+    private static void primeno(int n){
+        int count = 0;
+        if(n<2){
+            System.out.println("The number is prime number");
+        }
+        for(int i = 1; i <=n; i++){
+            if(n%i==0)
+                count+=1;
+        }
+        if(count> 2){
+            System.out.println("The number is prime");
+        }else
+            System.out.println("The number is not prime");
+    }
+}
