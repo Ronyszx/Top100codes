6c81c9b3afcf62c57e53fbd0455bff8399adfe12
Ronyszx <ronysouz7@gmail.com>
2022-05-06 18:39:58 +0530

    100codes

---
+++ src/ReverseNum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ReverseNum {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter your number");
+        int num = s.nextInt();
+        int rev = 0, rem;
+        while (num != 0){
+            rem = num % 10;
+            rev = rev * 10 + rem;
+            num /= 10;
+        }
+        System.out.println("Reversed Num\n" + rev);
+
+
+    }
+}
