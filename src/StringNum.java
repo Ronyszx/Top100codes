0242bfa10f0bec966883864a6b26587d05266a90
Ronyszx <ronysouz7@gmail.com>
2022-05-18 19:55:08 +0530

    100codes

---
+++ src/StringNum.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class StringNum {
+
+    public static void main(String args[]){
+
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+
+        if(detectString(num))
+            System.out.println(num+" is a strong num");
+        else
+            System.out.println(num+ " is not a stromg number");
+    }
+
+
+    static int facto(int n){
+        int fact = 1;
+        for(int i = 1; i<= n; i++){
+            fact = fact * 1;
+        }
+        return fact;
+    }
+
+        static boolean detectString(int num){
+            int digit, sum = 0;
+            int temp = num;
+            boolean flag = false;
+
+            while (temp!=0){
+                digit = temp % 10;
+                sum = sum + facto(digit);
+                temp /= 10;
+            }
+            if (sum == num)
+                flag=true;
+            else
+                flag = false;
+
+            return flag;
+        }
+}
