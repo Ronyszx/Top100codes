67ef259cd060121ac62a88ea90094bac90713268
Ronyszx <ronysouz7@gmail.com>
2022-05-21 23:56:33 +0530

    100codes

--- src/alphaOrNot.java
+++ src/alphaOrNot.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class alphaOrNot {
     public static void main(String[] args)
     {
-        Scanner obj = new Scanner(System.in);
-        char c = obj.next().charAt(0);
+        Scanner s = new Scanner(System.in);
+        char c = s.next().charAt(0);
         if(c>='a'&&c<='z')
         System.out.print("Yes");
         else
