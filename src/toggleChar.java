3be6f294e295ecb32951ff00e088a4bfc34db419
Ronyszx <ronysouz7@gmail.com>
2022-05-26 14:30:35 +0530

    100codes

---
+++ src/toggleChar.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class toggleChar {
+    public static void main(String[] args) {
+        String s = "PrePInsTa";
+        String s1 = "";
+        for (int i = 0; i < s.length(); i++) {
+            if(Character.isUpperCase(s.charAt(i)))
+                s1=s1+Character.toLowerCase(s.charAt(i));
+            else
+                s1=s1+Character.toUpperCase(s.charAt(i));
+        }
+        System.out.println("Toggle String is : "+s1);
+
+    }
+}
