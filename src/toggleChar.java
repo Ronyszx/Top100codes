7f9b6fe96fd90c6ee1c730906b88c7ec6985c760
Ronyszx <ronysouz7@gmail.com>
2022-05-26 14:32:27 +0530

    100codes

--- src/toggleChar.java
+++ src/toggleChar.java
@@ -2,13 +2,15 @@ import java.util.Scanner;
 
 public class toggleChar {
     public static void main(String[] args) {
-        String s = "PrePInsTa";
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the String");
+        String ss = s.next();
         String s1 = "";
-        for (int i = 0; i < s.length(); i++) {
-            if(Character.isUpperCase(s.charAt(i)))
-                s1=s1+Character.toLowerCase(s.charAt(i));
+        for (int i = 0; i < ss.length(); i++) {
+            if(Character.isUpperCase(ss.charAt(i)))
+                s1=s1+Character.toLowerCase(ss.charAt(i));
             else
-                s1=s1+Character.toUpperCase(s.charAt(i));
+                s1=s1+Character.toUpperCase(ss.charAt(i));
         }
         System.out.println("Toggle String is : "+s1);
 
