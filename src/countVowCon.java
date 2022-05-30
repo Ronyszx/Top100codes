5adb1a9d8a337de012c6e6911a0adf589e93d2ac
Ronyszx <ronysouz7@gmail.com>
2022-05-30 21:50:47 +0530

    Top 100 codes

---
+++ src/countVowCon.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class countVowCon {
+
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        String ss = s.nextLine();
+        char[] c = ss.toCharArray();
+        int vowel = 0;
+        int cons = 0;
+
+        for(int i = 0; i < ss.length(); i++){
+            if(ss.charAt(i)== 'a' | ss.charAt(i)== 'e' | ss.charAt(i)== 'i' | ss.charAt(i)== 'o'| ss.charAt(i)== 'u' ){
+                vowel++;
+            }
+        }
+        System.out.println(vowel);
+
+    }
+}
