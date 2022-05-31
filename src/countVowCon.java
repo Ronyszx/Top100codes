0fa337800a2f4e0d7e9276c7c5e24ad207451751
Ronyszx <ronysouz7@gmail.com>
2022-05-31 22:42:55 +0530

    Top 100 codes

--- src/countVowCon.java
+++ src/countVowCon.java
@@ -12,13 +12,14 @@ public class countVowCon {
         for(int i = 0; i < ss.length(); i++){
             if(ss.charAt(i)== 'a' | ss.charAt(i)== 'e' | ss.charAt(i)== 'i' | ss.charAt(i)== 'o'| ss.charAt(i)== 'u' ){
                 vowel++;
+                System.out.println(vowel);
             }
             if(ss.charAt(i)!= 'a' | ss.charAt(i)!= 'e' | ss.charAt(i)!= 'i' | ss.charAt(i)!= 'o'| ss.charAt(i) != 'u'){
                 cons++;
+                System.out.println(cons);
             }
         }
-        System.out.println(vowel);
-        System.out.println(cons);
+
 
     }
 }
