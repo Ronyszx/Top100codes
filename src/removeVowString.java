4215aba42979eebf274a583b4a06dc5264b4a839
Ronyszx <ronysouz7@gmail.com>
2022-06-03 22:57:31 +0530

    Top 100 codes

--- src/removeVowString.java
+++ src/removeVowString.java
@@ -5,7 +5,6 @@ public class removeVowString {
         Scanner s = new Scanner(System.in);
         String s1 = s.next();
         String s2= "";
-
         s2 = s1.replaceAll("[AEIOUaeiou]", "");
         System.out.println(s2);
     }
