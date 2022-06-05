25e1a1e9d1d04930cd40b66cfab06cd7b35201d7
Ronyszx <ronysouz7@gmail.com>
2022-06-05 22:54:37 +0530

    Top 100 codes

--- src/StringPalindrome.java
+++ src/StringPalindrome.java
@@ -1,15 +1,18 @@
+import java.util.Scanner;
+
 public class StringPalindrome {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the string");
+        String s1 = s.next();
+        String s2 = "";
 
-            String s = "arora";
-            String rev = "";
-            for (int i = s.length()-1; i >=0 ; i--)
-                rev=rev+s.charAt(i);
-            if(s.equals(rev))
-                System.out.println("String is palindrome");
-            else
-                System.out.println("String is not palindrome");
-
+        for(int i = s1.length()-1; i>= 0; i--){
+            s2 = s2 + s1.charAt(i);
         }
-
+        if(s1.equals(s2)){
+            System.out.println("String is palindrome");
+        }else
+            System.out.println("String is not palindrome");
+    }
 }
