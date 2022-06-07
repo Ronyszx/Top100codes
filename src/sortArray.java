f0b1b5af34fd6efb007adf968c2b6afcc9b5fc5e
Ronyszx <ronysouz7@gmail.com>
2022-06-07 23:15:14 +0530

    Top 100 codes

---
+++ src/sortArray.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class sortArray {
+    public static void main(String args[]){
+        int temp =0;
+        int array[]= {5,3,4,2,6,3,1,4,'A',5};
+        for(int i = 0; i< array.length ; i++){
+            for(int j = 0 ; j< array.length; j++){
+                if(array[i]<array[j]){
+                    temp = array[i];
+                    array[i]= array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        System.out.println("Elements in sorted order are:");
+        for(int i = 0; i< array.length; i++){
+            System.out.println(array[i]);
+        }
+    }
+}
