367559984c27dd2fd0e92ef19d682f7afd2389c4
Ronyszx <ronysouz7@gmail.com>
2022-06-26 21:16:35 +0530

    Top 100 codes

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,7 @@ import java.util.*;
 
 class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String s = scan.nextLine();
-        int sum = 0;
-        for(int i = 0 ; i < s.length(); i++){
-            if(Character.isDigit(s.charAt(i)))
-                sum+=(s.charAt(i)-'0');
-        }
-        System.out.println(sum);
+
+
     }
 }
