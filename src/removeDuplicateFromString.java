b187826818b08ecdf91f26b41e0adf59eabf75fb
Ronyszx <ronysouz7@gmail.com>
2022-07-08 00:01:10 +0530

    TopCodes

--- src/removeDuplicateFromString.java
+++ src/removeDuplicateFromString.java
@@ -1,8 +1,6 @@
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
-import java.util.TreeSet;
-
 public class removeDuplicateFromString {
     public static void main(String args[]){
         String name = "bhaskar";
@@ -10,7 +8,7 @@ public class removeDuplicateFromString {
         Set hs = new LinkedHashSet();
         for(int i = 0; i < c.length; i++){
             hs.add(c[i]);
-
+        }
         System.out.println(hs.toArray().length);
     }
 }
